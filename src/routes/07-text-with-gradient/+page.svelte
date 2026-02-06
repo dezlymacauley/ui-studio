15652ee38497ec7118ae5449c06889a31dceb8ad
dezlymacauley <dezlymacauley@proton.me>
2026-02-06 11:14:53 +0200

    Learned how to use bg-clip-text

---
+++ src/routes/07-text-with-gradient/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import favicon from "$lib/assets/svelte_logo.svg";
+  import "./page.css";
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<style>
+  @reference "./page.css";
+
+  .brand-heading {
+
+    /* Apply a left → right background gradient to the element box */
+    @apply bg-linear-to-r from-purple-500 to-fuchsia-700;
+
+    /* The text must be large and bold enough for you to see the effects */
+    @apply text-lg font-extrabold;
+
+    /* 
+      Make the inner color of the text transparent.
+    */
+    @apply text-transparent;
+
+    /*
+      This will ensure that the background gradient color only appears,
+      inside the letters of the text, 
+      instead of appearing across the entire h1.
+    */
+    @apply bg-clip-text;
+  }
+
+</style>
+
+<div class="">
+  <h1 class="brand-heading">Dezly Macauley</h1>
+</div>
